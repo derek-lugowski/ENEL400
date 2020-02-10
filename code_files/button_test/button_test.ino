501fe84b5026f6f8c61d0a7110cc51a24a5c07ce
derek-lugowski <derek-lugowski@users.noreply.github.com>
2020-02-10 13:30:42 -0700

    added button test file

---
+++ code_files/button_test/button_test.ino
@@ -0,0 +1,16 @@
+
+
+
+
+const int[6] buttonPin = {2,3,4,5,6,7}; // arduino pins where buttons are connected
+
+
+void setup() {
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
