9508cd5d2d8abe9db941bd82d2781ed4f2283fe4
derek-lugowski <58445275+derek-lugowski@users.noreply.github.com>
2020-03-06 14:08:55 -0700

    added more documentation

--- libraries/Controller/Controller.h
+++ libraries/Controller/Controller.h
@@ -2,12 +2,23 @@
 /*
  * This is a class for reading the 6 button controller.
  * 
- * Controller.read_buttons();
+ * int* button_states = Controller.read_buttons();
+ *
+ * button_states is a pointer to an array of 6 ints that corrispond to each of the buttons
+ * button_states --> {A, B, U, D, L, R}
+ *
  * checks the value of the buttons initialized with the Controler object.
  * This will check the state of the button and debounce it.
  * This should be called every game loop to check the button state.
  * May not update right away after button pressing due to debouncing.
  * 
+ *
+ * void start(int* button_pins);
+ *
+ * This function initializes the Controller object by:
+ * 1. setting the pins that the controller in connected to on the Arduino
+ * 2. setting the pinmode for the pins to INPUT
+ *
  */
 #ifndef CONTROLLER_H_INCLUDED
 #define CONTROLLER_H_INCLUDED
