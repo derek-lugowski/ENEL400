cf767ce3c10af1b3781b6cd1a1d4d1f4f1930329
derek-lugowski <derek-lugowski@users.noreply.github.com>
2020-03-07 17:11:00 -0700

    started minesweeper game

---
+++ libraries/Minesweeper/Minesweeper.cpp
@@ -0,0 +1,22 @@
+//Minesweeper.cpp
+
+#include <Minesweeper.h>
+#include <Controller.h>
+#include <MCUFRIEND_kbv.h>
+
+
+void Minesweeper(Controller controller, MCUFRIEND_kbv tft)
+{
+  
+  
+  while(1)
+  {
+    
+    
+    
+    if(1){break;}
+  }
+  
+  
+  
+}
---
+++ libraries/Minesweeper/Minesweeper.h
@@ -0,0 +1,12 @@
+//Minesweeper.h
+
+#ifndef MINESWEEPER_INCLUDED
+#define MINESWEEPER_INCLUDED
+
+#include <Controller.h>
+#include <MCUFRIEND_kbv.h>
+
+void Minesweeper(Controller controller, MCUFRIEND_kbv tft);
+
+
+#endif
