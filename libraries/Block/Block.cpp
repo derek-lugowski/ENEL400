de93ca20fb7c0446313a323fda28c150e998a47f
derek-lugowski <derek-lugowski@users.noreply.github.com>
2020-03-08 13:02:26 -0600

    new library files for minesweeper

---
+++ libraries/Block/Block.cpp
@@ -0,0 +1,12 @@
+//Block.cpp
+
+#include <Block.h>
+
+Block::Block()
+{
+  is_mine = false;
+  is_tested = false;
+  is_flagged = false;
+}
+
+
---
+++ libraries/Block/Block.h
@@ -0,0 +1,17 @@
+//Block.h
+
+#ifndef BLOCK_INCLUDED
+#define BLOCK_INCLUDED
+
+class Block
+{
+  public:
+    bool is_mine;
+    bool is_tested;
+    bool is_flagged;
+    
+    Block();
+};
+
+
+#endif
